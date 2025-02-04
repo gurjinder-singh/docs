c9941cb321227621fce300b3cd815719ba500bb0
gurjinder-singh <gurjinder@gesso.one>
2025-02-04 00:16:25 -0500

    Update BlogPost.vue

--- content/1.blog/layouts/BlogPost.vue
+++ content/1.blog/layouts/BlogPost.vue
@@ -1,1 +1,16 @@
-
+<script setup>
+  const { next } = useContent()
+    </script>
+<template>
+<div class="container">
+  <div class="content">
+    <slot />
+  </div>
+  <div v-if="next" class="next-link">
+      <div> Read next post </div>
+    <NuxtLink :to="next._path">
+      {{ next.title }}
+    </NuxtLink>
+    <p>{{ next.description }}</p>
+</div>
+</template>
